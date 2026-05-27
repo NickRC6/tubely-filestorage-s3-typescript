d3e84daccd4bbbafc1b8907da9a0b633843cca53
NickRC6 <nickrc6@gmail.com>
2026-05-27 22:28:39 +0700

    Added signed urls

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -2,9 +2,9 @@ import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
 import { getBearerToken, validateJWT } from "../auth";
-import type { BunRequest } from "bun";
+import { stringWidth, type BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import { getVideo, updateVideo } from "../db/videos";
+import { getVideo, updateVideo, type Video } from "../db/videos";
 import { randomBytes } from "crypto";
 
 function classifyRatio(ratio: number): string {
@@ -67,11 +67,12 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
     const s3File = cfg.s3Client.file(`${aspectRatio}/${filename}`);
     await s3File.write(Bun.file(processedVideo), { type: file.type });
 
-    const newURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${aspectRatio}/${filename}`
-    video.videoURL = newURL;
+    //const newURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${aspectRatio}/${filename}`
+    video.videoURL = `${aspectRatio}/${filename}`;
     
     updateVideo(cfg.db, video)
-    return respondWithJSON(200, video);
+    const signedVideo = await dbVideoToSignedVideo(cfg, video)
+    return respondWithJSON(200, signedVideo);
   } finally {
     await Bun.file(tempPath).delete();
     if (processedVideo) {
@@ -134,4 +135,19 @@ export async function processVideoForFastStart(inputFilePath: string) {
   }
 
   return outputFilePath;
+}
+
+export async function generatePresignedURL(cfg: ApiConfig, key: string, expireTime: number) {
+  const presignedURL = cfg.s3Client.presign(key, { expiresIn: expireTime });
+
+  return presignedURL;
+}
+
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video): Promise<Video> {
+  if (!video.videoURL) {
+    return video;
+  }
+
+  video.videoURL = await generatePresignedURL(cfg, video.videoURL, 900);
+  return video;
 }
